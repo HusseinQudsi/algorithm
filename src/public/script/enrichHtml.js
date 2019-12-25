0cba528f2dc797b8405e42f7931e26c1081e5728
hussein818 <hussein8844@gmail.com>
2019-12-25 01:27:53 -0500

    adding-code

--- src/public/script/enrichHtml.js
+++ src/public/script/enrichHtml.js
@@ -1,65 +1,95 @@
-if (typeof window !== 'undefined') {
+const arrAlgorithm = window.arrayAlgorithms();
+const numAlgorithm = window.numberAlgorithms()
 
-  const arrAlgorithm = window.arrayAlgorithms();
-  const numAlgorithm = window.numberAlgorithms()
+enrichTextArea();
+populateTextAreaAlgorithms();
+addGlobalTextAreaEventHandler();
 
-  // adding textArea:
-  const textareaTemplate = (`
-    <textarea name="name" rows="8" cols="80"></textarea>
-    <button type="button" name="button">click</button>
-  `);
+function enrichTextArea() {
 
-  const preTags = document.getElementsByTagName('pre');
+  try {
 
-  [].forEach.call(preTags, (el) => {
+    // adding textArea:
+    const preTags = document.getElementsByTagName('pre');
+    const textareaTemplate = (`
+      <textarea name="name" rows="8" cols="80"></textarea>
+      <button type="button" name="button">click</button>
+    `);
 
-    const div = document.createElement('div');
+    [].forEach.call(preTags, (el) => {
 
-    div.innerHTML = textareaTemplate;
-    el.parentElement.append(div);
-  });
+      const div = document.createElement('div');
 
-  const algorithms = Object.assign(arrAlgorithm, numAlgorithm);
-  const algorithmsMethodsNames = Object.keys(algorithms);
-  const sections = document.querySelectorAll('h3[id]');
+      div.innerHTML = textareaTemplate;
+      el.parentElement.append(div);
+    });
 
-  [].forEach.call(sections, (el) => {
+  } catch(error) {
+    // TODO logging
+    console.log('enrichTextArea: ' + error);
+    throw new Error(error);
+  }
+}
 
-    const name = el.innerText.replace(/^\w/, (w) => w.toLowerCase());
-    const method = algorithmsMethodsNames[algorithmsMethodsNames.indexOf(name)];
+function populateTextAreaAlgorithms() {
 
-    if (method) {
+  try {
 
-      const section = el.parentElement.parentElement;
-      const textarea = section.getElementsByTagName('textarea');
+    const algorithms = Object.assign(arrAlgorithm, numAlgorithm);
+    const algorithmsMethodsNames = Object.keys(algorithms);
+    const sections = document.querySelectorAll('h3[id]');
 
-      if (textarea.length) {
+    [].forEach.call(sections, (el) => {
+
+      const name = el.innerText.replace(/^\w/, (w) => w.toLowerCase());
+      const method = algorithmsMethodsNames[algorithmsMethodsNames.indexOf(name)];
+
+      if (method) {
+
+        const section = el.parentElement.parentElement;
+        const textarea = section.getElementsByTagName('textarea');
+
+        if (textarea.length) {
 
           const preTag = section.getElementsByTagName('pre')[0];
+
           textarea[0].innerHTML = preTag.innerText + "\n\n" + algorithms[method];
+        }
       }
-
-    }
-
-  });
-
-
-  const playgroundEventHandler = document.getElementsByTagName('button');
-
-  [].forEach.call(playgroundEventHandler, (el) => {
-
-    el.addEventListener('click', (event) => {
-
-      const section = event.target.parentElement.parentElement;
-      const name = section.getElementsByTagName('h3')[0].innerText;
-      const fncName = name.replace(/^\w/, (w) => w.toLowerCase());
-      const textarea = section.getElementsByTagName('textarea');
-      const preTag = section.getElementsByTagName('pre')[0];
-      const fnc = textarea[0].value;
-      // const fnc = preTag.innerText + textarea[0].value;
-
-      alert(eval(eval('fnc')));
     });
-  });
 
+  } catch(error) {
+    // TODO logging
+    console.log('populateTextAreaAlgorithms: ' + error);
+    throw new Error(error);
+  }
+}
+
+function addGlobalTextAreaEventHandler() {
+
+  try {
+
+    const playgroundEventHandler = document.getElementsByTagName('button');
+
+    [].forEach.call(playgroundEventHandler, (el) => {
+
+      el.addEventListener('click', (event) => {
+
+        const section = event.target.parentElement.parentElement;
+        const name = section.getElementsByTagName('h3')[0].innerText;
+        const fncName = name.replace(/^\w/, (w) => w.toLowerCase());
+        const textarea = section.getElementsByTagName('textarea');
+        const preTag = section.getElementsByTagName('pre')[0];
+        const fnc = textarea[0].value;
+        // const fnc = preTag.innerText + textarea[0].value;
+
+        alert(eval(eval('fnc')));
+      });
+    });
+
+  } catch(error) {
+    // TODO logging
+    console.log('addGlobalTextAreaEventHandler: ' + error);
+    throw new Error(error);
+  }
 }

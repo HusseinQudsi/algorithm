7736650dc440081b0b5e9cedfed8788ff1558ae0
hussein818 <hussein8844@gmail.com>
2020-10-12 16:01:47 -0400

    adding basic offline support application

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,7 @@
 import express from 'express'
-import path from 'path'
 import config from './config'
-import processHtml from './resources/processHtml'
 
 const app = express()
-const baseUrl = config.basePath + config.versioning
 const dir = __dirname
 
 // TODO: move to config
@@ -14,10 +11,6 @@ const fileConfig = {
 };
 
 app.use(express.static(dir + fileConfig.publicDir))
-
-const htmlFilePath = path.join(dir + fileConfig.index);
-
-processHtml(htmlFilePath);
 
 export const server = () => (
   app.listen(config.port, () => (
